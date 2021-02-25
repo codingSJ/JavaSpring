713661dba93c946683b5d4cca8b40de473d64af9
codingSJ <seojoon.hong94@gmail.com>
2021-02-25 12:47:18 +0900

    clear() 함수 추가 및 리포지토리 테스트 구현

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -5,14 +5,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
 //굳이 public으로 구현할 필요가 없다.
 /*public*/ class MemoryMemberRepositoryTest {
 
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach  //각 테스트가 끝난 뒤에 수행해주는 annotation
+    public void afterEach(){
+        repository.clear();
+    }
 
     //각 기능들을 테스트해준다//
     @Test
@@ -23,10 +27,16 @@ import static org.assertj.core.api.Assertions.*;
 
         //when: 생성한 member를 리포지토리에 저장한다.
         repository.save(member);
+        System.out.println("save()");
+        List<Member> all = repository.findAll();
+        for(Member mem:all) {
+            System.out.println("mem.getId() = " + mem.getId());
+            System.out.println("mem.getName() = " + mem.getName());
+        }
+
 
         //then: 리포지토리에서 찾은 result와 위에서 생성한 member가 동일한 객체여야 한다.
         Member result = repository.findById(member.getId()).get();
-        System.out.println("결과: " + (result == member));
         assertThat(member).isEqualTo(result);
     }
 
@@ -36,13 +46,17 @@ import static org.assertj.core.api.Assertions.*;
         Member member1 = new Member();
         member1.setName("test1");
         repository.save(member1);
-        System.out.println("member1.getId() = " + member1.getId());
 
         Member member2 = new Member();
         member2.setName("test2");
         repository.save(member2);
-        System.out.println("member2.getId() = " + member2.getId());
 
+        System.out.println("fbName()");
+        List<Member> all = repository.findAll();
+        for(Member mem:all) {
+            System.out.println("mem.getId() = " + mem.getId());
+            System.out.println("mem.getName() = " + mem.getName());
+        }
 
         //when: 저장된 객체 중에서 이름을 통해 검색한다.
         Member result1 = repository.findByName("test1").get();
@@ -64,7 +78,12 @@ import static org.assertj.core.api.Assertions.*;
         repository.save(member2);
 
         //when: 리포지토리에서 저장된 회원을 List로 갖고온다.
+        System.out.println("fAll");
         List<Member> result = repository.findAll();
+        for(Member mem:result) {
+            System.out.println("mem.getId() = " + mem.getId());
+            System.out.println("mem.getName() = " + mem.getName());
+        }
 
         //then: 리포지토리에서 찾은 result의 size는 2인지 확인한다.
         assertThat(result.size()).isEqualTo(2);
@@ -79,6 +98,13 @@ import static org.assertj.core.api.Assertions.*;
 
         //when: 리포지토리에서 ID를 통해 객체를 가져온다.
         Member result = repository.findById(member.getId()).get();
+
+        System.out.println("fbId()");
+        List<Member> all = repository.findAll();
+        for(Member mem:all) {
+            System.out.println("mem.getId() = " + mem.getId());
+            System.out.println("mem.getName() = " + mem.getName());
+        }
 
         //then: 리포지토리에서 찾은 result와 위에서 생성한 member가 동일한 객체여야 한다.
         assertThat(member.getId()).isEqualTo(result.getId());
