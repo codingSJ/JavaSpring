a4fdeebb82c49aabc74e1e1b20f998cdb7471e0f
codingSJ <seojoon.hong94@gmail.com>
2021-02-26 18:09:25 +0900

    회원 조희 기능

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,12 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -18,17 +22,17 @@ public class MemberController {
     }
 
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
     }
 
     @GetMapping("/members/new")
-    public String createForm(){
+    public String createForm() {
         return "members/createMemberForm";
     }
 
     @PostMapping("/members/new")
-    public String create(MemberForm memberForm){
+    public String create(MemberForm memberForm) {
         //memberForm의 name을 이름으로 갖는 새 Member 객체를 생성한다.
         Member member = new Member();
         member.setName(memberForm.getName());
@@ -37,5 +41,15 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String members(Model model) {
+        List<Member> members = memberService.findMembers();
+
+        //html 코드의 members라는 변수에 members를 사용한다.
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
